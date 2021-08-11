351d6cbb1be9d5302abededfdb8dc49de53ad981
Hunter Motko <huntermotko@Hunters-MacBook-Pro.local>
2021-08-11 15:29:32 -0400

    create react app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express()
-const port = 3005 
+/** @format */
 
-app.get('/', (req, res)=>{
+const express = require('express')
+const app = express()
+const port = 3005
+
+app.get('/', (req, res) => {
 	res.send('Welcome to Deadwood Tree Service')
 })
 
-app.listen(port, ()=> console.log(`Listening on ${port}`))
-
+app.listen(port, () => console.log(`Listening on ${port}`))
