39ac9c316704ab5d46f5fc9049bb135c696f84bc
hunterMotko <hunterMotko@gmail.com>
2021-08-11 15:45:55 +0000

    modify git ignore

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+const port = 3005 
+
+app.get('/', (req, res)=>{
+	res.send('Welcome to Deadwood Tree Service')
+})
+
+app.listen(port, ()=> console.log(`Listening on ${port}`))
+
